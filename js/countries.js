4de5ce67e7954f200b2d67aa95f0a5ef69bf19a9
hadar Avarjil <hadaravarjil@gmail.com>
2024-08-27 00:05:07 +0300

    Add some css to organize the display layout

--- js/countries.js
+++ js/countries.js
@@ -14,27 +14,19 @@ function onGetCountryInfo() {
 
 function renderInfo(data) {
     console.log(data[0].name.common);
-    
+
     console.log(data);
     const titileElement = document.querySelector('.country-name')
-    titileElement.innerHTML =  data[0].name.common
+    titileElement.innerHTML = data[0].name.common
 
     const imgElement = document.querySelector('.country-flag')
-    imgElement.src =   data[0].flags.png
-    
+    imgElement.src = data[0].flags.png
+
     const populaionField = document.querySelector('p.populaion')
-    populaionField.innerHTML = `The Country's population is `+ data[0].population + ' pepole.'
+    populaionField.innerHTML = `The Country's population is ` + data[0].population + ' pepole.'
 
     const areaField = document.querySelector('p.area')
-    areaField.innerHTML =  `The Country's area is `+ data[0].area+ ' km.'
-
-
-    // <h2 class="country-name"></h2>
-    // <img>
-    // <p class="populaion"></p>
-    // <p class="area"></p>
-
-
+    areaField.innerHTML = `The Country's area is ` + data[0].area + ' km.'
 
 }
 
