76d977898464d69f35dd14d4c06ebef4abd6d8e0
hadar Avarjil <hadaravarjil@gmail.com>
2024-08-26 19:19:29 +0300

    Implement renderInfo function

--- js/countries.js
+++ js/countries.js
@@ -1,9 +1,25 @@
 'use strict'
 
+
+
 function onGetCountryInfo() {
-    console.log('Hi')
+    const inputElement = document.querySelector('.txt-input')
+    const country = inputElement.value
+    console.log(country);
+    if (country) {
+        getCountry(country, renderInfo)
+    }
 }
 
+
+
 function renderInfo(data) {
-    console.log('Rendering...')
+    console.log(data);
+
+    const preElement = document.querySelector('.countryData')
+    preElement.innerHTML =  JSON.stringify(data)
+}
+
+function getCountry(country, cb) {
+    $.get(`https://restcountries.com/v3.1/name/${country}`, cb)
 }
